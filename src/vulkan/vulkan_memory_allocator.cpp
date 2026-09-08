70343857b9673f71e6d3ea634931a2c940795bb4
MateuszKuzniewski <kuzniewskii.mateusz@gmail.com>
2026-09-08 18:15:29 +0100

    [GET] added methods to allocate buffer and image

--- src/vulkan/vulkan_memory_allocator.cpp
+++ src/vulkan/vulkan_memory_allocator.cpp
@@ -1,4 +1,5 @@
 #include <stdexcept>
+#include <cstring>
 #include "vulkan_memory_allocator.h"
 
 get::vulkan_memory_allocator::vulkan_memory_allocator(VkInstance instance, VkPhysicalDevice physicalDevice, VkDevice device)
@@ -16,20 +17,67 @@ get::vulkan_memory_allocator::vulkan_memory_allocator(VkInstance instance, VkPhy
 
     vmaImportVulkanFunctionsFromVolk(&vmaAllocatorInfo, &vmaFunctionsInfo);
 
-    VkResult res = vmaCreateAllocator(&vmaAllocatorInfo, &_vma_allocator);
+    VkResult res = vmaCreateAllocator(&vmaAllocatorInfo, &_allocator);
     if (res != VK_SUCCESS)
     {
         throw std::runtime_error("SYSTEM: Failed to create VMA");
     }
 }
 
+get::gpu_buffer get::vulkan_memory_allocator::create_buffer(const VkBufferCreateInfo* bufferInfo, const VmaAllocationCreateInfo* allocInfo)
+{
+    get::gpu_buffer result {};
+    VkResult res = vmaCreateBuffer(_allocator, bufferInfo, allocInfo, &result.buffer, &result.allocation, nullptr);
+
+    if (res != VK_SUCCESS)
+    {
+        throw std::runtime_error("SYSTEM: Failed to create buffer");
+        return get::gpu_buffer{};
+    }
+
+    return result;
+}
+
+
+get::gpu_image get::vulkan_memory_allocator::create_image(const VkImageCreateInfo* createInfo, const VmaAllocationCreateInfo* allocInfo)
+{
+    get::gpu_image result {};
+    VkResult res = vmaCreateImage(_allocator, createInfo, allocInfo, &result.image, &result.allocation, nullptr);
+
+    if (res != VK_SUCCESS)
+    {
+        throw std::runtime_error("SYSTEM: Failed to create image");
+        return get::gpu_image{};
+    }
+
+    return result;
+}
+
+void get::vulkan_memory_allocator::copy_buffer_data(const get::gpu_buffer& buffer, size_t bufferOffset, void* data, size_t byteSize)
+{
+    void* bufferPtr = nullptr;
+    if (vmaMapMemory(_allocator, buffer.allocation, &bufferPtr) != VK_SUCCESS)
+    {
+        throw std::runtime_error("SYSTEM: Unable to map buffer memory");
+        return;
+    }
+
+    std::memcpy(static_cast<char*>(bufferPtr) + bufferOffset, data, byteSize);
+    vmaUnmapMemory(_allocator, buffer.allocation);
+}
+
+void get::vulkan_memory_allocator::destroy(VkBuffer buffer, VmaAllocation allocation)
+{
+    vmaDestroyBuffer(_allocator, buffer, allocation);
+}
+
 get::vulkan_memory_allocator::~vulkan_memory_allocator()
 {
-    if (_vma_allocator)
-        vmaDestroyAllocator(_vma_allocator);
+    if (_allocator)
+        vmaDestroyAllocator(_allocator);
 }
 
 VmaAllocator get::vulkan_memory_allocator::get_allocator() const
 {
-    return _vma_allocator;
+    return _allocator;
 }
--- src/vulkan/vulkan_memory_allocator.h
+++ src/vulkan/vulkan_memory_allocator.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <volk.h>
 #include <vk_mem_alloc.h>
+#include "render_data.h"
 
 namespace get
 {
@@ -12,11 +13,15 @@ namespace get
         ~vulkan_memory_allocator();
 
         [[nodiscard]] VmaAllocator get_allocator() const;
-
         
+        get::gpu_buffer create_buffer(const VkBufferCreateInfo* bufferInfo, const VmaAllocationCreateInfo* allocInfo);
+        get::gpu_image create_image(const VkImageCreateInfo* createInfo, const VmaAllocationCreateInfo* allocInfo);
+        
+        void copy_buffer_data(const get::gpu_buffer& buffer, size_t bufferOffset, void* data, size_t byteSize);
+        void destroy(VkBuffer buffer, VmaAllocation allocation);
 
     private:
 
-        VmaAllocator _vma_allocator;
+        VmaAllocator _allocator;
     };
 }
