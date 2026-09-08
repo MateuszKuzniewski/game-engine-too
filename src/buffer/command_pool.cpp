1a51bd45b81ae5aef2f7f24964ff276824f7f372
MateuszKuzniewski <kuzniewskii.mateusz@gmail.com>
2026-09-08 15:35:22 +0100

    [GET] added command pool for transient buffers

--- src/buffer/command_pool.cpp
+++ src/buffer/command_pool.cpp
@@ -3,20 +3,43 @@
 
 
 get::command_pool::command_pool(VkDevice device, const u32 queueFamilyIndex, std::vector<frame_resource>& resources)
+    : _device(device), _id(queueFamilyIndex)
 {
+    VkCommandPoolCreateInfo poolInfo
+    {
+        .sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO,
+            .flags = VK_COMMAND_POOL_CREATE_TRANSIENT_BIT,
+            .queueFamilyIndex = _id,
+    };
+
+    VkResult result = vkCreateCommandPool(_device, &poolInfo, nullptr, &_transient_command_pool); 
+    if (result != VK_SUCCESS)
+    {
+        throw std::runtime_error("SYSTEM: Failed to create a command pool");
+    }
 
     for (auto& res : resources)
     {
         VkCommandPoolCreateInfo poolInfo
         {
             .sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO,
-                .queueFamilyIndex = queueFamilyIndex
+                .queueFamilyIndex = _id
         };
 
-        VkResult result = vkCreateCommandPool(device, &poolInfo, nullptr, &res.command_pool);
+        VkResult result = vkCreateCommandPool(_device, &poolInfo, nullptr, &res.command_pool);
         if (result != VK_SUCCESS)
         {
             throw std::runtime_error("SYSTEM: Failed to create a command pool");
         }
     }
+}
+
+get::command_pool::~command_pool()
+{
+    vkDestroyCommandPool(_device, _transient_command_pool, nullptr);
+}
+
+VkCommandPool get::command_pool::get_transient_command_pool() const
+{
+    return _transient_command_pool;
 }
--- src/buffer/command_pool.h
+++ src/buffer/command_pool.h
@@ -10,7 +10,19 @@ namespace get
     {
     public:
 
-        command_pool(VkDevice device, const u32 queueFamilyIndex, std::vector<frame_resource>& res);
-        ~command_pool() = default;
+        command_pool(VkDevice device, const u32 queueFamilyIndex, std::vector<frame_resource>& resources);
+        ~command_pool();
+
+        void create_command_pool_resource(std::vector<frame_resource>& resources);
+        void create_command_pool_transient();
+
+        VkCommandPool get_transient_command_pool() const;
+
+    private:
+
+        VkDevice _device;
+        u32 _id;
+
+        VkCommandPool _transient_command_pool;
     };
 }
