c4495bb418ccfff05ac893c7db73d99d66edf9ba
MateuszKuzniewski <kuzniewskii.mateusz@gmail.com>
2026-09-10 16:59:32 +0100

    [GET] deleted frame_resources.h and moved contents to render_data

--- src/rendering/render_data.h
+++ src/rendering/render_data.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <volk.h>
 #include <glm/glm.hpp>
+#include <string>
+#include <vector>
 #include "vk_mem_alloc.h"
 #include "types.h"
 
@@ -8,10 +10,10 @@ namespace get
 {
     struct vertex
     {
-        glm::vec3 position;
-        glm::vec3 normal;
-        glm::vec3 color;
-        glm::vec2 uv;
+        glm::vec3 position = glm::vec3(0.0f);
+        glm::vec3 normal = glm::vec3(1.0f);
+        glm::vec3 color = glm::vec3(0.0f);
+        glm::vec2 uv = glm::vec2(0.0f);
     };
 
     struct image
@@ -40,5 +42,52 @@ namespace get
     {
         u32 image_id;
         u32 sampler_id;
+    };
+
+    struct material
+    {
+        glm::vec4 base_color;
+        u32 texture_id;
+    };
+
+    struct sub_mesh
+    {
+        size_t vertex_start;
+        size_t vertex_count;
+        size_t index_start;
+        size_t index_count;
+        u32 material_id;
+    };
+
+    struct mesh
+    {
+        std::string name;
+        std::vector<sub_mesh> sub_meshes;
+    };
+
+    struct frame_constants
+    {
+        u64 vertex_buffer_address;
+        u64 material_buffer_address;
+        u64 render_items_address;
+    };
+
+    struct render_item 
+    {
+        glm::mat4 wvp;
+        glm::mat4 world_matrix;
+        u32 material_index;
+    };
+
+    struct frame_resource 
+    {
+        VkCommandPool command_pool = nullptr;
+        VkCommandBuffer command_buffer = nullptr;
+        VkSemaphore image_acquired_semaphore = nullptr;
+        VkDescriptorSet descriptor_set = nullptr;
+        get::gpu_buffer indirect_draw_buffer;
+        get::gpu_buffer render_item_buffer;
+        VkDrawIndexedIndirectCommand* indirect_draw_ptr = nullptr;
+        render_item* render_item_ptr;
     };
 }
