d8c93d679bae7a5f2d8882ab81e0c8769abf0c8b
MateuszKuzniewski <kuzniewskii.mateusz@gmail.com>
2026-09-11 14:34:35 +0100

    [GET] changed default vertex color to white

--- src/rendering/render_data.h
+++ src/rendering/render_data.h
@@ -11,7 +11,7 @@ namespace get
     struct vertex
     {
         glm::vec3 position = glm::vec3(0.0f);
-        glm::vec3 color = glm::vec3(0.0f);
+        glm::vec3 color = glm::vec3(1.0f);
         glm::vec3 normal = glm::vec3(1.0f);
         glm::vec2 uv = glm::vec2(0.0f);
     };
