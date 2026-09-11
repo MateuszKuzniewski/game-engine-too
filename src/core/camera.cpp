38e904bea2edda214847e4dccc13d08380f1845a
MateuszKuzniewski <kuzniewskii.mateusz@gmail.com>
2026-09-11 14:35:30 +0100

    [GET] removed camera rotation

--- src/core/camera.cpp
+++ src/core/camera.cpp
@@ -24,15 +24,15 @@ void get::camera::update(u32 width, u32 height)
 void get::camera::calculate_perspective(f64 ratio)
 {
     // TO DO: Check perspectiveRH
-    _projection_matrix = glm::perspective(glm::radians(_settings.fov), ratio, _settings.near_clip, _settings.far_clip);
+    _projection_matrix = glm::perspectiveRH(glm::radians(_settings.fov), ratio, _settings.near_clip, _settings.far_clip);
 }
 
 void get::camera::calculate_view()
 {
     // pitch / yaw / roll
-    _roll += glm::radians(10.f) * frame_time::delta_time();
-    glm::quat rot = glm::vec3(0.0f, 0.0f, _roll);
-    glm::vec3 pos = glm::vec3(0.0f, 0.0f, 2.0f); 
+    // _roll += glm::radians(10.f) * frame_time::delta_time();
+    glm::quat rot = glm::vec3(0.0f, 0.0f, 0.0f);
+    glm::vec3 pos = glm::vec3(0.0f, 0.0f, 0.0f); 
 
     _view_matrix = glm::translate(glm::mat4(1.0f), pos) * glm::mat4(rot);
     _view_matrix = glm::inverse(_view_matrix);
