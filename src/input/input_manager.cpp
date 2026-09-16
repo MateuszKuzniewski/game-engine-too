1c25f4e6a384cedebbaeae3389e93884c5edf111
MateuszKuzniewski <kuzniewskii.mateusz@gmail.com>
2026-09-16 11:51:43 +0100

    [GET] added missing movement and rotation axis

--- src/input/input_manager.cpp
+++ src/input/input_manager.cpp
@@ -7,16 +7,20 @@ get::input_manager::input_manager(const window& win, camera& cam)
     glfwSetKeyCallback(win.get_current_window(), input_manager::key_callback);
     
     // camera -> move
-    _input[GLFW_KEY_W] =        [this]() { _camera.move(world::forward);    };
-    _input[GLFW_KEY_S] =        [this]() { _camera.move(world::backward);   };
-    _input[GLFW_KEY_A] =        [this]() { _camera.move(world::left);       };
-    _input[GLFW_KEY_D] =        [this]() { _camera.move(world::right);      };
-    _input[GLFW_KEY_SPACE] =    [this]() { _camera.move(world::up);         };
-    _input[GLFW_KEY_C] =        [this]() { _camera.move(world::down);       };
-
+    _input[GLFW_KEY_W] =        [this]() { _camera.move(_camera.get_view_dir());    };
+    _input[GLFW_KEY_S] =        [this]() { _camera.move(-_camera.get_view_dir());   };
+    _input[GLFW_KEY_A] =        [this]() { _camera.move(-_camera.get_right_dir());  };
+    _input[GLFW_KEY_D] =        [this]() { _camera.move(_camera.get_right_dir());   };
+    _input[GLFW_KEY_SPACE] =    [this]() { _camera.move(world::up); };
+    _input[GLFW_KEY_C] =        [this]() { _camera.move(world::down); };
+    
     // camera -> rotate
-    _input[GLFW_KEY_LEFT] =     [this]() { _camera.rotate(5.0f, world::up); };
-    _input[GLFW_KEY_RIGHT] =    [this]() { _camera.rotate(-5.0f, world::up); };
+    _input[GLFW_KEY_LEFT] =     [this]() { _camera.rotate(5.0f, world::up);       };
+    _input[GLFW_KEY_RIGHT] =    [this]() { _camera.rotate(-5.0f, world::up);      }; 
+    _input[GLFW_KEY_UP] =       [this]() { _camera.rotate(5.0f, world::right);    };
+    _input[GLFW_KEY_DOWN] =     [this]() { _camera.rotate(-5.0f, world::right);   };
+    _input[GLFW_KEY_E] =        [this]() { _camera.rotate(5.0f, world::forward);  };
+    _input[GLFW_KEY_Q] =        [this]() { _camera.rotate(-5.0f, world::forward); };
 }
 
 void get::input_manager::update()
@@ -47,4 +51,3 @@ void get::input_manager::key_press_event(i32 key, i32 action)
         _held_keys.erase(key);
     }
 }
-
