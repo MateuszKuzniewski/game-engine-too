f89a7464c1e6abf847c6b586e54144a9fbb38673
MateuszKuzniewski <kuzniewskii.mateusz@gmail.com>
2026-09-17 16:16:08 +0100

    [GET] initialized imgui for vulkan

--- src/UI/gui.cpp
+++ src/UI/gui.cpp
@@ -1,6 +1,111 @@
 #include "gui.h"
+#include <imgui_impl_vulkan.h>
+#include <imgui_impl_glfw.h>
+#include <iostream>
 
-get::gui::gui()
+get::gui::gui(
+        const window& win,
+        VkFormat format,
+        VkInstance instance,
+        VkPhysicalDevice physicalDevice,
+        VkDevice device,
+        u32 queueFamilyID,
+        VkQueue queue,
+        u32 swapchainImageCount) : _device(device)
 {
 
+    // From ImGui example code
+    VkDescriptorPoolSize pool_sizes[] =
+    {
+        { VK_DESCRIPTOR_TYPE_SAMPLED_IMAGE, IMGUI_IMPL_VULKAN_MINIMUM_SAMPLED_IMAGE_POOL_SIZE },
+        { VK_DESCRIPTOR_TYPE_SAMPLER, IMGUI_IMPL_VULKAN_MINIMUM_SAMPLER_POOL_SIZE },
+    };
+
+    VkDescriptorPoolCreateInfo pool_info = {};
+    pool_info.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_POOL_CREATE_INFO;
+    pool_info.flags = VK_DESCRIPTOR_POOL_CREATE_FREE_DESCRIPTOR_SET_BIT;
+    pool_info.maxSets = 0;
+    for (VkDescriptorPoolSize& pool_size : pool_sizes)
+        pool_info.maxSets += pool_size.descriptorCount;
+    pool_info.poolSizeCount = (uint32_t)IM_COUNTOF(pool_sizes);
+    pool_info.pPoolSizes = pool_sizes;
+
+    VkResult err = vkCreateDescriptorPool(_device, &pool_info, nullptr, &_imgui_pool);
+    check_vk_result(err);
+            
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGui::StyleColorsDark();
+    ImGui_ImplGlfw_InitForVulkan(win.get_current_window(), true);
+        
+    VkPipelineRenderingCreateInfoKHR pipelineRenderInfo
+    {
+        .sType = VK_STRUCTURE_TYPE_PIPELINE_RENDERING_CREATE_INFO_KHR,
+        .colorAttachmentCount = 1,
+        .pColorAttachmentFormats = &format
+    };
+
+    ImGui_ImplVulkan_PipelineInfo pipelineInfo
+    {
+        .Subpass = 0,
+        .MSAASamples = VK_SAMPLE_COUNT_1_BIT,
+        .PipelineRenderingCreateInfo = pipelineRenderInfo,
+    };
+
+    ImGui_ImplVulkan_InitInfo initInfo
+    {
+        .Instance = instance,
+        .PhysicalDevice = physicalDevice,
+        .Device = device,
+        .QueueFamily = queueFamilyID,
+        .Queue = queue,
+        .DescriptorPool = _imgui_pool,
+        .MinImageCount = swapchainImageCount,
+        .ImageCount = swapchainImageCount,
+        .PipelineInfoMain = pipelineInfo,
+        .UseDynamicRendering = true,
+        .CheckVkResultFn = check_vk_result
+    };
+    
+    ImGui_ImplVulkan_Init(&initInfo);
+}
+
+void get::gui::setup()
+{
+    ImGui_ImplVulkan_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+    ImGui::NewFrame();
+}
+
+void get::gui::render(VkCommandBuffer commandBuffer)
+{
+    setup();
+    
+    ImGui::Begin("Debug");
+    ImGui::Text("Hello from ImGui");
+    ImGui::End();
+
+    ImGui::Render();
+
+    ImGui_ImplVulkan_RenderDrawData(ImGui::GetDrawData(), commandBuffer);
+}
+
+get::gui::~gui()
+{
+    ImGui_ImplVulkan_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+    vkDestroyDescriptorPool(_device, _imgui_pool, nullptr);
+}
+
+void get::gui::check_vk_result(VkResult err) 
+{
+    if (err == VK_SUCCESS) 
+        return;
+        
+    std::cerr << "SYSTEM: ImGui ERROR VkResult = " << err << std::endl;
+    if (err < 0) 
+    {
+        abort(); 
+    }
 }
--- src/UI/gui.h
+++ src/UI/gui.h
@@ -1,13 +1,41 @@
 #pragma once
 #include <volk.h>
 #include <imgui.h>
+#include "window.h"
 
 namespace get
 {
     class gui
     {
     public:
-        gui();
-        ~gui() = default;
+
+        gui(const window& win,
+            VkFormat format,
+            VkInstance instance,
+            VkPhysicalDevice physicalDevice,
+            VkDevice device,
+            u32 queueFamilyID,
+            VkQueue queue,
+            u32 swapchainImageCount);
+
+        ~gui();
+        
+        gui(const gui&) = delete;
+        gui(gui&&) = delete;
+        gui& operator=(const gui&) = delete;
+        gui& operator=(gui&&) = delete;
+    
+        void render(VkCommandBuffer commandBuffer);
+
+    
+    private:
+
+        static void check_vk_result(VkResult err);
+        void setup();
+
+    private:
+
+        VkDevice _device;
+        VkDescriptorPool _imgui_pool;
     };
 }
